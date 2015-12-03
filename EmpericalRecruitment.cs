8180763846d42e60ce083c13044e4c5412c00854
Eric Fletcher <eric.fletcher@noaa.gov>
2015-12-02 16:06:06 -1000

    Added RecruitmentModel Base Class for Recruitment Classes.
    Began implmenting EmpericalRecruitment operations

--- EmpericalRecruitment.cs
+++ EmpericalRecruitment.cs
@@ -3,16 +3,117 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.IO;
 
 namespace AGEPRO_struct
 {
-    public class EmpericalRecruitment : AGEPRO_Recruitment
+    public class EmpericalRecruitment : RecruitmentModel
     {
-   
+        public int numObs { get; set; }
+        public DataTable obsTable { get; set; }
+        public bool withSSB { get; set; }
+
+        public EmpericalRecruitment()
+        {
+            this.recruitCategory = 1;
+            this.withSSB = false;
+        }
+
+        public EmpericalRecruitment(bool useSSB)
+        {
+            this.recruitCategory = 1;
+            this.withSSB = useSSB;
+        }
+
+        protected override void ReadRecruitmentModel(StreamReader sr)
+        {
+            string line;
+            
+
+            //numObs
+            line = sr.ReadLine();
+            this.numObs = Convert.ToInt32(line);
+
+            //obsTable
+            this.obsTable = ReadObsTable(sr, this.withSSB);
+
+
+        }
+
+        protected DataTable ReadObsTable(StreamReader sr, bool useSSB)
+        {
+            string line;
+            DataTable inputTable = new DataTable();
+
+            //obsTable
+            line = sr.ReadLine();
+            string[] nobsRecruits = line.Split(' ');
+            if (useSSB)
+            {
+                line = sr.ReadLine();
+                string[] nobsSSB = line.Split(' ');
+                for (int i = 0; i < numObs; i++)
+                {
+                    inputTable.Rows.Add((i + 1), Convert.ToDouble(nobsRecruits[i]), Convert.ToDouble(nobsSSB[i]));
+                }
+                
+            }
+            else
+            {
+                for (int i = 0; i < numObs; i++)
+                {
+                    inputTable.Rows.Add((i + 1), Convert.ToDouble(nobsRecruits[i]));
+                }
+                
+            }
+
+            return inputTable;
+        }
 
         public class TwoStageEmpericalRecruitment : EmpericalRecruitment
         {
+            public int lv1NumObs { get; set; }
+            public int lv2NumObs { get; set; }
+            public int SSBBreakVal { get; set; }
+            public DataTable lv1Obs { get; set; }
+            public DataTable lv2Obs { get; set; }
 
+            public TwoStageEmpericalRecruitment()
+            {
+                this.recruitCategory = 1;
+                this.withSSB = true; //TODO: Should this be default?
+            }
+
+            public TwoStageEmpericalRecruitment(bool useSSB)
+            {
+                this.recruitCategory = 1;
+                this.withSSB = useSSB;
+            }
+
+
+            protected override void ReadRecruitmentModel(StreamReader sr)
+            {
+                string line;
+                DataTable inputTable = new DataTable();
+
+                //lv1NumObs, lv2NumObs
+                line = sr.ReadLine();
+                string[] lineNumObsLvl = line.Split(' ');
+                this.lv1NumObs = Convert.ToInt32(lineNumObsLvl[0]);
+                this.lv2NumObs = Convert.ToInt32(lineNumObsLvl[1]);
+
+                //lv1Obs 
+                this.lv1Obs = base.ReadObsTable(sr, this.withSSB);
+                //lv2Obs
+                this.lv2Obs = base.ReadObsTable(sr, this.withSSB);
+
+                //SSBBReakVal
+                line = sr.ReadLine();
+                this.SSBBreakVal = Convert.ToInt32(line);
+            }
         }
-    }
-}
+
+        
+    } 
+ }
---
+++ RecruitmentModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AGEPRO_struct
+{
+    public class RecruitmentModel
+    {
+        public int recruitModelNum { get; set; }
+        public int recruitCategory { get; set; }
+
+        public RecruitmentModel()
+        {
+
+        }
+
+        public string GetRecruitModelName(int modelType)
+        {
+            //TODO:Use Dictionary to get Model Name  
+
+            return "";
+        }
+
+        protected virtual void ReadRecruitmentModel(StreamReader sr)
+        {
+            //Generic Recruitment Model does not have inupt file strucutre. 
+            //TODO: Should throw exception or do nothing?
+            throw new Exception("Recruitment Model is not a valid AGEPRO Recruitment model.");
+        }
+    }
+}
