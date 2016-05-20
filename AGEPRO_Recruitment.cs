2aac50eb38dea12f8473efaa337ee1210d819926
Eric Fletcher <eric.fletcher@noaa.gov>
2016-05-20 10:47:03 -1000

    AGEPRO_recruitment.cs code cleanup Documentation Changes

--- AGEPRO_Recruitment.cs
+++ AGEPRO_Recruitment.cs
@@ -8,11 +8,14 @@ using System.IO;
 
 namespace AGEPRO_struct
 {
+    /// <summary>
+    /// General AGEPRO Recruitment Parameters. 
+    /// </summary>
     public class AGEPRO_Recruitment
     {
         public int recruitScalingFactor { get; set; }
         public int SSBScalingFactor { get; set; }
-        public int maxRecuitObs { get; set; }
+        public int maxRecruitObs { get; set; }
         public int[] recruitType { get; set; }
         public DataTable recruitProb = new DataTable("Recruitment Probability");
         public int recruitmentCategory { get; set; }
@@ -24,6 +27,12 @@ namespace AGEPRO_struct
             
         }
         
+        /// <summary>
+        /// Reads in AGEPRO Input File for Recruitment Model Data
+        /// </summary>
+        /// <param name="sr">AGEPRO Input File StreamReader</param>
+        /// <param name="nyears">Number of Years in projection</param>
+        /// <param name="numRecruitModels">Number of Recruitment models</param>
         public void ReadRecruitmentData(StreamReader sr, int nyears, int numRecruitModels)
         {
             string line;
@@ -35,7 +44,7 @@ namespace AGEPRO_struct
             string[] recruitOpt = line.Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
             this.recruitScalingFactor = Convert.ToInt32(recruitOpt[0]); //Recruitment Scaling Factor
             this.SSBScalingFactor = Convert.ToInt32(recruitOpt[1]); //SSB Scaling Factor
-            this.maxRecuitObs = Convert.ToInt32(recruitOpt[2]);
+            this.maxRecruitObs = Convert.ToInt32(recruitOpt[2]);
 
             //Recruit Methods
             line = sr.ReadLine().Trim();
@@ -75,7 +84,6 @@ namespace AGEPRO_struct
                 
             }
 
-            Console.WriteLine("Reading Recuitment Model Data ... ");
             
             //Instanciate recruitList as new list
             recruitList = new List<RecruitmentModel>();
@@ -90,8 +98,10 @@ namespace AGEPRO_struct
             if (recruitList.Count(r => r.recruitModelNum == 1) > 1)
             {
                 throw new ArgumentException("Multiple Markov Matrix Recruitment not allowed");
-            }   
-
+            }
+            
+            Console.WriteLine("Reading Recuitment Model Data ... ");
+            
             //Read Recruitment Data
             foreach (var nrecruit in recruitList)
             {
@@ -101,60 +111,64 @@ namespace AGEPRO_struct
             Console.WriteLine("Done.");
         }
         
-        
-        private void AddToRecruitList(int recruitType, List<RecruitmentModel> recruitList)
+        /// <summary>
+        /// Creates a new recruitment model, based on type (model number). Then adds the object to the Recruitment Model Class List.
+        /// </summary>
+        /// <param name="rtype">Recruitment Model Number</param>
+        /// <param name="rlist">Recruitment Model List Object</param>
+        private void AddToRecruitList(int rtype, List<RecruitmentModel> rlist)
         {
             
-            switch (recruitType)
+            switch (rtype)
             {
                 case 1:
-                    recruitList.Add(new MarkovMatrixRecruitment());
+                    rlist.Add(new MarkovMatrixRecruitment());
                     break;
                 case 2:
-                    recruitList.Add(new EmpericalRecruitment(recruitType, useSSB: true)); 
+                    rlist.Add(new EmpericalRecruitment(rtype, useSSB: true)); 
                     break;
                 case 3: 
                 case 14:
                 case 20:
-                    recruitList.Add(new EmpericalRecruitment(recruitType, useSSB: false));
+                    rlist.Add(new EmpericalRecruitment(rtype, useSSB: false));
                     break;
                 case 4:
                     
-                    recruitList.Add(new TwoStageEmpericalRecruitment(recruitType, useSSB: true));
+                    rlist.Add(new TwoStageEmpericalRecruitment(rtype, useSSB: true));
                     break;
                 case 5:
                 case 6:
                 case 7:
-                    recruitList.Add(new ParametricCurve(recruitType, isAutocorrelated: false));
+                    rlist.Add(new ParametricCurve(rtype, isAutocorrelated: false));
                     break;
                 case 8:
-                    recruitList.Add(new ParametricLognormal(recruitType, isAutocorrelated: false));
+                    rlist.Add(new ParametricLognormal(rtype, isAutocorrelated: false));
                     break;
                 case 10:
                 case 11:
                 case 12:
-                    recruitList.Add(new ParametricCurve(recruitType, isAutocorrelated: true));
+                    rlist.Add(new ParametricCurve(rtype, isAutocorrelated: true));
                     break;
                 case 13:
-                    recruitList.Add(new ParametricLognormal(recruitType, isAutocorrelated: true));
+                    rlist.Add(new ParametricLognormal(rtype, isAutocorrelated: true));
                     break;
                 case 15:
-                    recruitList.Add(new TwoStageEmpericalRecruitment(recruitType, useSSB: false));
+                    rlist.Add(new TwoStageEmpericalRecruitment(rtype, useSSB: false));
                     break;
                 case 16:
                 case 17:
                 case 18:
                 case 19:
-                    PredictorRecruitment predictorRecruitModel = new PredictorRecruitment(recruitType);
+                    PredictorRecruitment predictorRecruitModel = new PredictorRecruitment(rtype);
                     predictorRecruitModel.obsYears = this.observationYears;
-                    recruitList.Add(predictorRecruitModel);
+                    rlist.Add(predictorRecruitModel);
                     break;
                 case 21:
-                    recruitList.Add(new EmpericalCDFZero(recruitType));
+                    rlist.Add(new EmpericalCDFZero(rtype));
                     break;
                 case 0:
                 default:
-                    throw new ArgumentOutOfRangeException("recruitType","Must be a vaild recruitType model number.");  
+                    throw new ArgumentOutOfRangeException("rtype","Must be a vaild recruitType model number.");  
             }//end switch
 
             
