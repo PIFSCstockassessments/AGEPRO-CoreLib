f517748c1c247ad38fecde679def82a26e8a0fdb
Eric Fletcher <eric.fletcher@noaa.gov>
2016-05-23 13:33:15 -1000

    MarkovRecruitment: Store DataTables in the DataSet

--- MarkovMatrixRecruitment.cs
+++ MarkovMatrixRecruitment.cs
@@ -12,10 +12,8 @@ namespace AGEPRO_struct
     {
         public int numRecruitLevels { get; set; }
         public int numSSBLevels { get; set; }
-        public DataTable recruitmentTable { get; set; }
-        public DataTable SSBTable { get; set; }
-        public DataTable probabilityTable { get; set; }
-
+        public DataSet markovRecruitment { get; set; }
+        
         public MarkovMatrixRecruitment()
         {
             this.recruitModelNum = 1; //Model 1 only for MarkovMatrix
@@ -24,61 +22,67 @@ namespace AGEPRO_struct
 
         public override void ReadRecruitmentModel(StreamReader sr)
         {
-            string line;
-            double SSBLevelProbSum;
-            double precisionDiff;
+            using (DataSet markov = new DataSet("markovRecruitmentTables"))
+            {
 
-            line = sr.ReadLine();
-            string[] MarkovMatrixOptions = line.Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                string line;
+                double SSBLevelProbSum;
+                double precisionDiff;
 
-            this.numRecruitLevels = Convert.ToInt32(MarkovMatrixOptions[0]);
-            this.numSSBLevels = Convert.ToInt32(MarkovMatrixOptions[1]);
+                line = sr.ReadLine();
+                string[] MarkovMatrixOptions = line.Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
 
-            //Recruitment
-            DataTable inputTable = new DataTable("Recruitment");
-            line = sr.ReadLine();
-            string[] inputTableLine = line.Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
-            inputTable.Columns.Add("Recruitment", typeof(int));
-            for (int i=0; i < this.numRecruitLevels; i++)
-            {
-                inputTable.Rows.Add(Convert.ToInt32(inputTableLine[i]));
-            }
-            this.recruitmentTable = inputTable;
-            
-            //SSB
-            inputTable = new DataTable("SSB");
-            line = sr.ReadLine();
-            inputTableLine = line.Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
-            inputTable.Columns.Add("SSB Cut Points", typeof(int));
-            for (int i = 0; i < this.numSSBLevels; i++)
-            {
-                inputTable.Rows.Add(Convert.ToInt32(inputTableLine[i]));
-            }
-            this.SSBTable = inputTable;
-            
-            //Probalility
-            inputTable = new DataTable("Probalitity");
-            for (int j = 0; j < this.numRecruitLevels; j++)
-            {
-                inputTable.Columns.Add("PR("+(j+1).ToString()+")",typeof(double));
-            }
-            for (int i = 0; i < this.numSSBLevels; i++)
-            {
+                this.numRecruitLevels = Convert.ToInt32(MarkovMatrixOptions[0]);
+                this.numSSBLevels = Convert.ToInt32(MarkovMatrixOptions[1]);
+
+                //Recruitment
+                DataTable inputTable = new DataTable("Recruitment");
+                line = sr.ReadLine();
+                string[] inputTableLine = line.Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                inputTable.Columns.Add("Recruitment", typeof(int));
+                for (int i = 0; i < this.numRecruitLevels; i++)
+                {
+                    inputTable.Rows.Add(Convert.ToInt32(inputTableLine[i]));
+                }
+                markov.Tables.Add(inputTable);
+
+                //SSB
+                inputTable = new DataTable("SSB");
                 line = sr.ReadLine();
                 inputTableLine = line.Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
-                
-                //Check Probability of each SSB Level (row) must sum to 1.0 
-                SSBLevelProbSum = Array.ConvertAll<string, double>(inputTableLine, double.Parse).Sum();
-                precisionDiff = Math.Abs(SSBLevelProbSum * .000001);
-                //To Handle Floating-Point Precision issues in "SSBLevelProbSum != 1" Comparision
-                if(!(Math.Abs(SSBLevelProbSum - (double)1.0) <= precisionDiff))
+                inputTable.Columns.Add("SSB Cut Points", typeof(int));
+                for (int i = 0; i < this.numSSBLevels; i++)
                 {
-                    throw new InvalidOperationException("SSB level " + (i + 1).ToString() + " probability sum does not equal to 1.0: " +
-                        "Probability sum is " + SSBLevelProbSum.ToString());
+                    inputTable.Rows.Add(Convert.ToInt32(inputTableLine[i]));
                 }
-                inputTable.Rows.Add(inputTableLine);
+                markov.Tables.Add(inputTable);
+
+                //Probalility
+                inputTable = new DataTable("Probalitity");
+                for (int j = 0; j < this.numRecruitLevels; j++)
+                {
+                    inputTable.Columns.Add("PR(" + (j + 1).ToString() + ")", typeof(double));
+                }
+                for (int i = 0; i < this.numSSBLevels; i++)
+                {
+                    line = sr.ReadLine();
+                    inputTableLine = line.Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+
+                    //Check Probability of each SSB Level (row) must sum to 1.0 
+                    SSBLevelProbSum = Array.ConvertAll<string, double>(inputTableLine, double.Parse).Sum();
+                    precisionDiff = Math.Abs(SSBLevelProbSum * .000001);
+                    //To Handle Floating-Point Precision issues in "SSBLevelProbSum != 1" Comparision
+                    if (!(Math.Abs(SSBLevelProbSum - (double)1.0) <= precisionDiff))
+                    {
+                        throw new InvalidOperationException("SSB level " + (i + 1).ToString() + " probability sum does not equal to 1.0: " +
+                            "Probability sum is " + SSBLevelProbSum.ToString());
+                    }
+                    inputTable.Rows.Add(inputTableLine);
+                }
+                markov.Tables.Add(inputTable);
+
+                this.markovRecruitment = markov;
             }
-            this.probabilityTable = inputTable;
         }
 
         
