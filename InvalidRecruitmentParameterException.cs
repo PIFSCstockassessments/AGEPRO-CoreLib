9f80ce57c1039422959b6ae8324a3db5170a03bf
Eric Fletcher <eric.fletcher@noaa.gov>
2016-09-13 12:23:21 -1000

    Added InvalidRecruitmentParameterException

---
+++ InvalidRecruitmentParameterException.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace AGEPRO.CoreLib
+{
+    [Serializable]
+    public class InvalidRecruitmentParameterException : Exception
+    {
+        public InvalidRecruitmentParameterException()
+        {
+
+        }
+        public InvalidRecruitmentParameterException(string message)
+            : base(message)
+        {
+
+        }
+        public InvalidRecruitmentParameterException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+
+        }
+        public InvalidRecruitmentParameterException(SerializationInfo info, StreamingContext c)
+            : base(info, c)
+        {
+
+        }
+    }
+}
