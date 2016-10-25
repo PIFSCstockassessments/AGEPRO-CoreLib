fc19d0cfcbc3349188e762446505b3bc4a7375b3
Eric Fletcher <eric.fletcher@noaa.gov>
2016-10-24 14:39:37 -1000

    Added HarvestSpecification Class

---
+++ HarvestSpecification.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AGEPRO.CoreLib
+{
+    public class HarvestSpecification
+    {
+        public int Index { get; set; }
+        public string HarvestScenario { get; set; }
+
+        public HarvestSpecification(int i, string spec)
+        {
+            Index = i;
+            HarvestScenario = spec;
+        }
+
+        private static readonly List<HarvestSpecification> harvestSpec = new List<HarvestSpecification>
+        {
+            { new HarvestSpecification(0, "F-MULT") },
+            { new HarvestSpecification(1, "LANDINGS") },
+            { new HarvestSpecification(2, "REMOVALS") }
+        };
+
+        public static List<HarvestSpecification> GetHarvestSpec()
+        {
+            return harvestSpec;
+        }
+
+    }
+}
