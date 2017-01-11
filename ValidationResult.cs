2226d9d5de8d2a70c7d0af6f1fe77085c2484e6f
Eric Fletcher <eric.fletcher@noaa.gov>
2017-01-11 10:48:07 -1000

    IValidate -> IValidatable

--- ValidationResult.cs
+++ ValidationResult.cs
@@ -4,10 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AGEPRO.CoreLib
+namespace Nmfs.Agepro.CoreLib
 {
-
-    public interface IValidate
+    public interface IValidatable
     {
         ValidationResult Validate();
     }
