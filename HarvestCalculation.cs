499d8a76741d16a7b02e78929c52296dc2c2b6ae
Eric Fletcher <eric.fletcher@noaa.gov>
2019-06-24 15:44:45 -1000

    Refactor HarvestCalucation abstract class to inherit AgeproCoreLibProperty.

--- HarvestCalculation.cs
+++ HarvestCalculation.cs
@@ -13,7 +13,7 @@ namespace Nmfs.Agepro.CoreLib
     /// <summary>
     /// Generalized, abstract represetion for (Non-Standard) Harvest Scenario Calculations
     /// </summary>
-    abstract public class HarvestCalculation : INotifyPropertyChanged, IValidatable
+    abstract public class HarvestCalculation : AgeproCoreLibProperty, IValidatable
     {
         public HarvestScenarioAnalysis calculationType;
         public int[] obsYears;
@@ -21,37 +21,6 @@ namespace Nmfs.Agepro.CoreLib
         public abstract List<string> WriteCalculationDataLines();
 
 
-        public event PropertyChangedEventHandler PropertyChanged;
-
-
-        /// <summary>
-        ///     Checks if a property already matches a desired value.  Sets the property and
-        ///     notifies listeners only when necessary. In AGEPRO, allows a GUI object to set
-        ///     values with its the data-binded CoreLib property.
-        /// </summary>
-        /// <remarks>
-        ///     Derived From DanRigby
-        /// </remarks>
-        /// <typeparam name="T">Type of the property.</typeparam>
-        /// <param name="field">Reference to a property with both getter and setter.</param>
-        /// <param name="value">Desired value for the property.</param>
-        /// <param name="name">
-        ///     Name of the property used to notify listeners.  This
-        ///     value is optional and can be provided automatically when invoked from compilers that
-        ///     support CallerMemberName.
-        /// </param>
-        protected void SetProperty<T>(ref T field, T value, [CallerMemberName] string name = "")
-        {
-            if (!EqualityComparer<T>.Default.Equals(field, value))
-            {
-                field = value;
-                var handler = PropertyChanged;
-                if (handler != null)
-                {
-                    handler(this, new PropertyChangedEventArgs(name));
-                }
-            }
-        }
 
         public virtual ValidationResult ValidateInput()
         {
