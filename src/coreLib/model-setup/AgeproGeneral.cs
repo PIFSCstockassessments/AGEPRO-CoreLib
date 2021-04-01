533bc1bf4e3289a8623457c9ed5c1ba960c97fa6
efletcherPIFSC <37851243+efletcherPIFSC@users.noreply.github.com>
2021-03-31 15:46:36 -1000

    Refactor AgeproGeneral code

--- src/coreLib/model-setup/AgeproGeneral.cs
+++ src/coreLib/model-setup/AgeproGeneral.cs
@@ -1,70 +1,63 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
 
 namespace Nmfs.Agepro.CoreLib
 {
-    /// <summary>
-    /// General Paramemeters of AGEPRO
-    /// </summary>
-    public class AgeproGeneral
+  /// <summary>
+  /// General Paramemeters of AGEPRO
+  /// </summary>
+  public class AgeproGeneral
+  {
+    public int ProjYearStart { get; set; }    //First Year in Projection
+    public int ProjYearEnd { get; set; }     //Last Year in Projection
+    public int AgeBegin { get; set; }        //First Age Class
+    public int AgeEnd { get; set; }           //Last Age Class
+    public int NumFleets { get; set; }       //Number of Fleets
+    public int NumRecModels { get; set; }    //Number of Recruit Models
+    public int NumPopSims { get; set; }      //Number of Population Simulations
+    public bool HasDiscards { get; set; }     //Discards are Present
+    public int Seed { get; set; }            //Random Number Seed
+    public string InputFile { get; set; }
+
+    public AgeproGeneral()
     {
-        public int projYearStart { get; set; }    //First Year in Projection
-        public int projYearEnd { get; set; }     //Last Year in Projection
-        public int ageBegin { get; set; }        //First Age Class
-        public int ageEnd { get; set; }           //Last Age Class
-        public int numFleets { get; set; }       //Number of Fleets
-        public int numRecModels { get; set; }    //Number of Recruit Models
-        public int numPopSims { get; set; }      //Number of Population Simulations
-        public bool hasDiscards { get; set; }     //Discards are Present
-        public int seed { get; set; }            //Random Number Seed
-        public string inputFile { get; set; }
 
-        public AgeproGeneral()
-        {
-
-        }
-
-        public AgeproGeneral(string file) 
-        {
-            this.inputFile = file;
-            //readin file contents
-        }
-        
-        /// <summary>
-        /// Determine number of years in projection by the (absolulte) diffefence between the 
-        /// last and first year of projection. 
-        /// </summary>
-        /// <returns>The difference stored in 'nYears'</returns>
-        public int NumYears()
-        {
-            int nYears = Math.Abs(this.projYearEnd - this.projYearStart) + 1;
-            return nYears;
-        }
-
-        /// <summary>
-        /// Determine number of ages in projection by the (absolulte) diffefence between last age 
-        /// class and first age class of projection. 
-        /// </summary>
-        /// <returns>The difference in stored in 'nAges'</returns>
-        public int NumAges()
-        {
-            int nAges = Math.Abs(this.ageBegin - this.ageEnd) + 1;
-            return nAges;
-        }
-
-        /// <summary>
-        /// Returns a sequence of years from First year of projection
-        /// </summary>
-        /// <returns>Returns a int array from <paramref name="projYearStart"/> by <paramref name="NumYears"/></returns>
-        public int[] SeqYears()
-        {
-            return Enumerable.Range(this.projYearStart, this.NumYears()).ToArray();
-        }
-
-        
     }
+
+    public AgeproGeneral(string file)
+    {
+      InputFile = file; //readin file contents
+    }
+
+    /// <summary>
+    /// Determine number of years in projection by the (absolulte) diffefence between the 
+    /// last and first year of projection. 
+    /// </summary>
+    /// <returns>The difference stored in 'nYears'</returns>
+    public int NumYears()
+    {
+      return Math.Abs(ProjYearEnd - ProjYearStart) + 1;
+    }
+
+    /// <summary>
+    /// Determine number of ages in projection by the (absolulte) diffefence between last age 
+    /// class and first age class of projection. 
+    /// </summary>
+    /// <returns>The difference in stored in 'nAges'</returns>
+    public int NumAges()
+    {
+      return Math.Abs(AgeBegin - AgeEnd) + 1;
+    }
+
+    /// <summary>
+    /// Returns a sequence of years from First year of projection
+    /// </summary>
+    /// <returns>Returns a int array from <paramref name="projYearStart"/> by <paramref name="NumYears"/></returns>
+    public int[] SeqYears()
+    {
+      return Enumerable.Range(ProjYearStart, NumYears()).ToArray();
+    }
+
+
+  }
 }
