bb65633af986cd5e58c5519e6968b4ccd5b8982b
efletcherPIFSC <37851243+efletcherPIFSC@users.noreply.github.com>
2021-04-05 14:25:00 -1000

    Refactor RecruitmentModel code

--- src/coreLib/recruitment/RecruitmentModel.cs
+++ src/coreLib/recruitment/RecruitmentModel.cs
@@ -1,110 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
 
 namespace Nmfs.Agepro.CoreLib
 {
+  /// <summary>
+  /// Generalized, abstract representation of AGEPRO Recruitment Model property
+  /// </summary>
+  abstract public class RecruitmentModel : AgeproCoreLibProperty, IValidatable
+  {
+    public int recruitModelNum;
+    public int recruitCategory;
+    public int[] obsYears;
+    public abstract void ReadRecruitmentModel(StreamReader sr);
+    public abstract List<string> WriteRecruitmentDataModelData();
+    public abstract ValidationResult ValidateInput();
+
     /// <summary>
-    /// Generalized, abstract representation of various AGEPRO Recruitment Models
+    /// Checks if this DataTable has any cell that may be blank, null, or with only white-space 
+    /// characters.
     /// </summary>
-    abstract public class RecruitmentModel : INotifyPropertyChanged, IValidatable
+    /// <returns>If the function finds an single instance of a blank/null/white-space only cell,
+    /// then it will return true. Otherwise, false.</returns>
+    protected virtual bool HasBlankOrNullCells(System.Data.DataTable table)
     {
-        public int recruitModelNum;
-        public int recruitCategory;
-        public int[] obsYears;
-        public abstract void ReadRecruitmentModel(StreamReader sr);
-        public abstract List<string> WriteRecruitmentDataModelData();
-        public abstract ValidationResult ValidateInput();
-        
-        public event PropertyChangedEventHandler PropertyChanged;
+      bool blankNullsExist = false;
 
-        /// <summary>
-        ///     Checks if a property already matches a desired value.  Sets the property and
-        ///     notifies listeners only when necessary. In AGEPRO, allows a GUI object to set
-        ///     values with its the data-binded CoreLib property.
-        /// </summary>
-        /// <remarks>
-        ///     Derived From DanRigby
-        /// </remarks>
-        /// <typeparam name="T">Type of the property.</typeparam>
-        /// <param name="field">Reference to a property with both getter and setter.</param>
-        /// <param name="value">Desired value for the property.</param>
-        /// <param name="name">
-        ///     Name of the property used to notify listeners.  This
-        ///     value is optional and can be provided automatically when invoked from compilers that
-        ///     support CallerMemberName.
-        /// </param>
-        protected void SetProperty<T>(ref T field, T value, [CallerMemberName] string name = "")
+      //Return true if there is no table, or 0 row or columns.
+      if (table == null)
+      {
+        return true;
+      }
+
+      foreach (System.Data.DataRow dtRow in table.Rows)
+      {
+        foreach (var item in dtRow.ItemArray)
         {
-            if (!EqualityComparer<T>.Default.Equals(field, value))
-            {
-                field = value;
-                var handler = PropertyChanged;
-                if (handler != null)
-                {
-                    handler(this, new PropertyChangedEventArgs(name));
-                }
-            }
+          if (string.IsNullOrWhiteSpace(item.ToString()))
+          {
+            blankNullsExist = true;
+          }
         }
+      }
 
-        /// <summary>
-        /// Checks if this DataTable has any cell that may be blank, null, or with only white-space 
-        /// characters.
-        /// </summary>
-        /// <returns>If the function finds an single instance of a blank/null/white-space only cell,
-        /// then it will return true. Otherwise, false.</returns>
-        protected virtual bool HasBlankOrNullCells(System.Data.DataTable table)
-        {
-            bool blankNullsExist = false;
-
-            //Return true if there is no table, or 0 row or columns.
-            if (table == null)
-            {
-                return blankNullsExist = true;
-            }
-
-            foreach (System.Data.DataRow dtRow in table.Rows)
-            {
-                foreach (var item in dtRow.ItemArray)
-                {
-                    if (string.IsNullOrWhiteSpace(item.ToString()))
-                    {
-                        blankNullsExist = true;
-                    }
-                }
-            }
-
-            return blankNullsExist;
-        }
-
-        /// <summary>
-        /// Checks to see if all the cells in the DataTable are siginificant value. Siginificance values
-        /// is defined by the value above the lower bound.  
-        /// </summary>
-        /// <param name="dtable">Data table to compare.</param>
-        /// <param name="lowBound">The lower bound that defines insigfiicance.</param>
-        /// <returns>If the function finds any value that is smaller than the lower bound
-        /// then it returns false.</returns>
-        protected bool TableHasAllSignificantValues(System.Data.DataTable dtable, double lowBound = 0.0001)
-        {
-            foreach (System.Data.DataRow drow in dtable.Rows)
-            {
-                foreach (System.Data.DataColumn dcol in dtable.Columns)
-                {
-                    if (Convert.ToDouble(drow[dcol]) < lowBound)
-                    {
-                        return false;
-                    }
-                }
-            }
-            return true;
-        }
-
-        
+      return blankNullsExist;
     }
+
+    /// <summary>
+    /// Verifies if all DataTable values are siginificantly above the lower bound.  
+    /// </summary>
+    /// <param name="dtable">Data table to compare.</param>
+    /// <param name="lowBound">The lower bound that defines insigfiicance.</param>
+    /// <returns>If the function finds any value that is smaller than the lower bound
+    /// then it returns false.</returns>
+    protected bool TableHasAllSignificantValues(System.Data.DataTable dtable, double lowBound = 0.0001)
+    {
+      foreach (System.Data.DataRow drow in dtable.Rows)
+      {
+        foreach (System.Data.DataColumn dcol in dtable.Columns)
+        {
+          if (Convert.ToDouble(drow[dcol]) < lowBound)
+          {
+            return false;
+          }
+        }
+      }
+      return true;
+    }
+
+
+  }
 }
