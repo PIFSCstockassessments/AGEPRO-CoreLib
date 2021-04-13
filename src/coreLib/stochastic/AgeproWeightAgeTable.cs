5022cde336c065f973c970c5bfbe4e2a362a6e0b
efletcherPIFSC <37851243+efletcherPIFSC@users.noreply.github.com>
2021-04-13 13:15:39 -1000

    Simplify and refactor out 'this'

--- src/coreLib/stochastic/AgeproWeightAgeTable.cs
+++ src/coreLib/stochastic/AgeproWeightAgeTable.cs
@@ -34,21 +34,21 @@ namespace Nmfs.Agepro.CoreLib
     {
       if (optParam.Equals("0"))
       {
-        this.FromFile = false; //0=User Spec by Age
-        this.weightOpt = Convert.ToInt32(optParam);
+        FromFile = false; //0=User Spec by Age
+        weightOpt = Convert.ToInt32(optParam);
       }
       else if (optParam.Equals("1"))
       {
-        this.FromFile = true; //1=From File
-        this.weightOpt = Convert.ToInt32(optParam);
+        FromFile = true; //1=From File
+        weightOpt = Convert.ToInt32(optParam);
       }
       else
       {
-        this.FromFile = null;
+        FromFile = null;
         //Check if weightOpt is a valid one
-        if (this.validOpt.Contains(Convert.ToInt32(optParam)))
+        if (validOpt.Contains(Convert.ToInt32(optParam)))
         {
-          this.weightOpt = Convert.ToInt32(optParam);
+          weightOpt = Convert.ToInt32(optParam);
         }
         else
         {
@@ -65,7 +65,7 @@ namespace Nmfs.Agepro.CoreLib
     {
       //If Option 0=fromFile, read fromFile options
       //otherwise ignore and finish reading the current AGEPRO parameter
-      if (this.weightOpt == 0)
+      if (weightOpt == 0)
       {
         base.ReadStochasticAgeTableFilename(sr);
       }
@@ -78,38 +78,38 @@ namespace Nmfs.Agepro.CoreLib
       List<string> outputLines = new List<string>();
 
       outputLines.Add(keyword); //[PARAMETER]
-      outputLines.Add(this.weightOpt.ToString() + new string(' ', 2) + Convert.ToInt32(this.TimeVarying).ToString());
+      outputLines.Add(weightOpt.ToString() + new string(' ', 2) + Convert.ToInt32(TimeVarying).ToString());
       //since fromFile is a nullable boolean, have to explicitly check if its true 
-      if (this.FromFile == true)
+      if (FromFile == true)
       {
-        outputLines.Add(this.DataFile);
+        outputLines.Add(DataFile);
       }
       // 0 == User Specfied Weights at Age
-      else if (this.weightOpt == 0)
+      else if (weightOpt == 0)
       {
 
-        if (this.ByAgeData == null)
+        if (ByAgeData == null)
         {
           throw new NullReferenceException("Stochastic Age of " +
               keyword + " is NULL.");
         }
-        if (this.ByAgeCV == null)
+        if (ByAgeCV == null)
         {
           throw new NullReferenceException("Stochastic CV of " +
               keyword + " is NULL.");
         }
 
-        foreach (DataRow yearRow in this.ByAgeData.Rows)
+        foreach (DataRow yearRow in ByAgeData.Rows)
         {
           outputLines.Add(string.Join(new string(' ', 2), yearRow.ItemArray));
         }
 
-        foreach (DataRow cvRow in this.ByAgeCV.Rows)
+        foreach (DataRow cvRow in ByAgeCV.Rows)
         {
           outputLines.Add(string.Join(new string(' ', 2), cvRow.ItemArray));
         }
       }
-      else if (!(this.validOpt.Contains(this.weightOpt)))
+      else if (!(validOpt.Contains(weightOpt)))
       {
         throw new InvalidOperationException("Invalid Weight option.");
       }
