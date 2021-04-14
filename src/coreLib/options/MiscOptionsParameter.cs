6314879c522d2a86ad8d07452c4942ac3010fa8a
efletcherPIFSC <37851243+efletcherPIFSC@users.noreply.github.com>
2021-04-14 09:56:50 -1000

    Inherit from AgeproCoreLibPropery class

--- src/coreLib/options/MiscOptionsParameter.cs
+++ src/coreLib/options/MiscOptionsParameter.cs
@@ -1,55 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Data;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
+﻿using System.Data;
 
 namespace Nmfs.Agepro.CoreLib
 {
   /// <summary>
   /// Wrapper class for AGEPRO's MiscOptions paramerers that includes two-way data binding. 
   /// </summary>
-  abstract public class MiscOptionsParameter : INotifyPropertyChanged
+  public abstract class MiscOptionsParameter : AgeproCoreLibProperty
   {
-
-    public event PropertyChangedEventHandler PropertyChanged;
 
     public MiscOptionsParameter()
     {
     }
-
-    /// <summary>
-    ///     Checks if a property already matches a desired value. Sets the property and
-    ///     notifies listeners only when necessary.
-    ///     In AGEPRO, allows a GUI object to set values with its the data-binded CoreLib
-    ///     property.
-    /// </summary>
-    /// <remarks>Derived From DanRigby</remarks>
-    /// <typeparam name="T">Type of the property.</typeparam>
-    /// <param name="field">Reference to a property with both getter and setter.</param>
-    /// <param name="value">Desired value for the property.</param>
-    /// <param name="name">
-    ///     Name of the property used to notify listeners.  This
-    ///     value is optional and can be provided automatically when invoked from compilers that
-    ///     support CallerMemberName.
-    /// </param>
-    protected void SetProperty<T>(ref T field, T value, [CallerMemberName] string name = "")
-    {
-      if (!EqualityComparer<T>.Default.Equals(field, value))
-      {
-        field = value;
-        var handler = PropertyChanged;
-        if (handler != null)
-        {
-          handler(this, new PropertyChangedEventArgs(name));
-        }
-      }
-    }
   }
-
 
   /// <summary>
   /// Parameters for the Reference Point Threshold Report
