22949fda38695661a74a026445c7695697d72e9d
efletcherPIFSC <37851243+efletcherPIFSC@users.noreply.github.com>
2021-04-13 16:30:43 -1000

    Fix Formatting (AgeproMiscOptions, MiscOptionsParameter)

--- src/coreLib/options/AgeproMiscOptions.cs
+++ src/coreLib/options/AgeproMiscOptions.cs
@@ -9,31 +9,31 @@ using System.Runtime.CompilerServices;
 
 namespace Nmfs.Agepro.CoreLib
 {
-    /// <summary>
-    /// AGEPRO Misc Options.
-    /// </summary>
-    public class AgeproMiscOptions
+  /// <summary>
+  /// AGEPRO Misc Options.
+  /// </summary>
+  public class AgeproMiscOptions
+  {
+    public bool enableSummaryReport { get; set; }
+    public bool enableAuxStochasticFiles { get; set; }
+    public bool enableExportR { get; set; }
+    //enabled if classes are called.
+    public bool enableRefpoint { get; set; }
+    public bool enablePercentileReport { get; set; }
+    public bool enableScaleFactors { get; set; }
+    public bool enableBounds { get; set; }
+    public bool enableRetroAdjustmentFactors { get; set; }
+
+    public AgeproMiscOptions()
     {
-        public bool enableSummaryReport { get; set; }
-        public bool enableAuxStochasticFiles { get; set; }
-        public bool enableExportR { get; set; }
-        //enabled if classes are called.
-        public bool enableRefpoint { get; set; }
-        public bool enablePercentileReport { get; set; }
-        public bool enableScaleFactors { get; set; }
-        public bool enableBounds { get; set; }
-        public bool enableRetroAdjustmentFactors { get; set; }
-
-        public AgeproMiscOptions()
-        {
-            enableRefpoint = false;
-            enablePercentileReport = false;
-            enableScaleFactors = false; 
-            enableBounds = false;
-            enableRetroAdjustmentFactors = false;
-        }
-
+      enableRefpoint = false;
+      enablePercentileReport = false;
+      enableScaleFactors = false;
+      enableBounds = false;
+      enableRetroAdjustmentFactors = false;
     }
+
+  }
 
 
 }
