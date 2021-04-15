3df6b859a30dea1f05f9bfd975819133889b8151
efletcherPIFSC <37851243+efletcherPIFSC@users.noreply.github.com>
2021-04-15 11:53:29 -1000

    Fix Formatting Refactor

--- src/util/Extensions.cs
+++ src/util/Extensions.cs
@@ -6,30 +6,30 @@ using System.Threading.Tasks;
 
 namespace Nmfs.Agepro.CoreLib
 {
-    public static class Extensions
+  public static class Extensions
+  {
+    public static T[] SplitArray<T>(this T[] array, int index, int length)
     {
-        public static T[] SplitArray<T>(this T[] array, int index, int length)
-        {
-            T[] slice = new T[length];
-            Array.Copy(array, index, slice, 0, length);
-            return slice;
-        }
-
-        public static System.Data.DataTable FillDBNullCellsWithZero (System.Data.DataTable dt)
-        {
-            for (int irow = 0 ; irow < dt.Rows.Count; irow++ )
-            {
-                foreach (System.Data.DataColumn jcol in dt.Columns)
-                {
-                    if ( dt.Rows[irow][jcol] == DBNull.Value)
-                    {
-                        dt.Rows[irow][jcol] = 0;
-                    }
-                }
-            }
-            return dt;
-        }
-
-        
+      T[] slice = new T[length];
+      Array.Copy(array, index, slice, 0, length);
+      return slice;
     }
+
+    public static System.Data.DataTable FillDBNullCellsWithZero(System.Data.DataTable dt)
+    {
+      for (int irow = 0; irow < dt.Rows.Count; irow++)
+      {
+        foreach (System.Data.DataColumn jcol in dt.Columns)
+        {
+          if (dt.Rows[irow][jcol] == DBNull.Value)
+          {
+            dt.Rows[irow][jcol] = 0;
+          }
+        }
+      }
+      return dt;
+    }
+
+
+  }
 }
