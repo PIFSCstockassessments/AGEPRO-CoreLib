dc4efa566c98488f9e453094791f086b70ef0c9e
efletcherPIFSC <37851243+efletcherPIFSC@users.noreply.github.com>
2021-04-14 14:40:29 -1000

    Fix Formatting Validation code refactor

--- src/validation/ValidatableExtensions.cs
+++ src/validation/ValidatableExtensions.cs
@@ -1,79 +1,77 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Nmfs.Agepro.CoreLib
 {
-    //poco-validation
-    public static class ValidatableExtensions
+  //poco-validation
+  public static class ValidatableExtensions
+  {
+    /// <summary>
+    /// Returns bool value of the Validation Check 
+    /// </summary>
+    /// <param name="input"></param>
+    /// <returns></returns>
+    public static bool IsValid(this IValidatable input)
     {
-        /// <summary>
-        /// Returns bool value of the Validation Check 
-        /// </summary>
-        /// <param name="input"></param>
-        /// <returns></returns>
-        public static bool IsValid(this IValidatable input)
-        {
-            return input.ValidationCheck().isValid;
-        }
+      return input.ValidationCheck().isValid;
+    }
 
-        /// <summary>
-        /// Retruns Message of the Vaildation Check
-        /// </summary>
-        /// <param name="input"></param>
-        /// <returns></returns>
-        public static string ValidationMessage(this IValidatable input)
-        {
-            return input.ValidationCheck().message;
-        }   
+    /// <summary>
+    /// Retruns Message of the Vaildation Check
+    /// </summary>
+    /// <param name="input"></param>
+    /// <returns></returns>
+    public static string ValidationMessage(this IValidatable input)
+    {
+      return input.ValidationCheck().message;
+    }
 
-        /// <summary>
-        /// Null check for nullable validatable objects 
-        /// </summary>
-        /// <param name="input"></param>
-        /// <returns></returns>
-        public static ValidationResult ValidationCheck(this IValidatable input)
-        {
-            if (input != null)
-            {
-                return input.ValidateInput();
-            }
-            else
-            {
-                return new ValidationResult(false, "Null or Missing input.");
-            }
-
-        }
-        
-        /// <summary>
-        /// Centralizes boilerplate needed to convert list of errors into single string.
-        /// </summary>
-        /// <param name="input"></param>
-        /// <returns></returns>
-        public static ValidationResult EnumerateValidationResults(this IEnumerable<string> input)
-        {
-            if (input == null)
-            {
-                return new ValidationResult(false, "Null or Missing input.");
-            }
-
-            //Enumerate the Errors
-            var errors = input.ToList();
-            var success = !errors.Any();
-            string message;
-            if (success)
-            {
-                message = "Validation Sucessful";
-            }
-            else
-            {
-                message = string.Join(Environment.NewLine, errors);
-            }
-
-            return new ValidationResult(success, message);
-        }
+    /// <summary>
+    /// Null check for nullable validatable objects 
+    /// </summary>
+    /// <param name="input"></param>
+    /// <returns></returns>
+    public static ValidationResult ValidationCheck(this IValidatable input)
+    {
+      if (input != null)
+      {
+        return input.ValidateInput();
+      }
+      else
+      {
+        return new ValidationResult(false, "Null or Missing input.");
+      }
 
     }
+
+    /// <summary>
+    /// Centralizes boilerplate needed to convert list of errors into single string.
+    /// </summary>
+    /// <param name="input"></param>
+    /// <returns></returns>
+    public static ValidationResult EnumerateValidationResults(this IEnumerable<string> input)
+    {
+      if (input == null)
+      {
+        return new ValidationResult(false, "Null or Missing input.");
+      }
+
+      //Enumerate the Errors
+      var errors = input.ToList();
+      var success = !errors.Any();
+      string message;
+      if (success)
+      {
+        message = "Validation Sucessful";
+      }
+      else
+      {
+        message = string.Join(Environment.NewLine, errors);
+      }
+
+      return new ValidationResult(success, message);
+    }
+
+  }
 }
--- src/validation/ValidationResult.cs
+++ src/validation/ValidationResult.cs
@@ -1,26 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Nmfs.Agepro.CoreLib
+﻿namespace Nmfs.Agepro.CoreLib
 {
-    public interface IValidatable
+  public interface IValidatable
+  {
+    ValidationResult ValidateInput();
+  }
+
+  public class ValidationResult
+  {
+    public bool isValid { get; private set; }
+    public string message { get; private set; }
+
+    public ValidationResult(bool valid, string message)
     {
-        ValidationResult ValidateInput();
+      this.isValid = valid;
+      this.message = message;
     }
 
-    public class ValidationResult
-    {
-        public bool isValid { get; private set; }
-        public string message { get; private set; }
-
-        public ValidationResult(bool valid, string message)
-        {
-            this.isValid = valid;
-            this.message = message;
-        }
-
-    }
+  }
 }
