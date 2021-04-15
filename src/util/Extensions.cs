3d2847f07af71428d8329cdf79fa1ccf5bc468cd
efletcherPIFSC <37851243+efletcherPIFSC@users.noreply.github.com>
2021-04-15 13:41:08 -1000

    Extensions Code cleanup; datatable null check

--- src/util/Extensions.cs
+++ src/util/Extensions.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Nmfs.Agepro.CoreLib
 {
@@ -17,6 +13,11 @@ namespace Nmfs.Agepro.CoreLib
 
     public static System.Data.DataTable FillDBNullCellsWithZero(System.Data.DataTable dt)
     {
+      if (dt is null)
+      {
+        throw new ArgumentNullException(nameof(dt));
+      }
+
       for (int irow = 0; irow < dt.Rows.Count; irow++)
       {
         foreach (System.Data.DataColumn jcol in dt.Columns)
@@ -29,7 +30,5 @@ namespace Nmfs.Agepro.CoreLib
       }
       return dt;
     }
-
-
-  }
+  } 
 }
