aeb18d51e7129f7f9425c04529218c468f140547
efletcherPIFSC <37851243+efletcherPIFSC@users.noreply.github.com>
2021-04-23 15:08:37 -1000

    Remove Unused Usings Code Refactor

--- src/coreLib/harvest/HarvestCalculation.cs
+++ src/coreLib/harvest/HarvestCalculation.cs
@@ -1,19 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
 
 
 namespace Nmfs.Agepro.CoreLib
 {
-    /// <summary>
-    /// Generalized, abstract represetion for (Non-Standard) Harvest Scenario Calculations
-    /// </summary>
-    abstract public class HarvestCalculation : AgeproCoreLibProperty, IValidatable
+  /// <summary>
+  /// Generalized, abstract represetion for (Non-Standard) Harvest Scenario Calculations
+  /// </summary>
+  abstract public class HarvestCalculation : AgeproCoreLibProperty, IValidatable
     {
         public HarvestScenarioAnalysis calculationType;
         public int[] obsYears;
