2cca926b35c2197c1fe758c353aa9f1e426c6846
Eric Fletcher <37851243+efletcherPIFSC@users.noreply.github.com>
2024-05-31 16:01:01 -1000

    Added StockSummaryFlag Enum

---
+++ src/coreLib/options/StockSummaryFlag.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nmfs.Agepro.CoreLib.src.coreLib.options
+{
+  public enum StockSummaryFlag
+  {
+    None = 0,
+    SummaryOnly = 1,
+    SummaryPlusAux2_10 = 2,
+    SummaryPlusAllAux = 3
+  }
+}
