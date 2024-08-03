8998ac5fcb94a8f5f4e8270e8de59f73e9e94582
Eric Fletcher <37851243+efletcherPIFSC@users.noreply.github.com>
2024-08-02 14:55:23 -1000

    Rename StockSummaryFlag -> AuxiliaryOutputFlag; Refactor enum to fit new changes to value

---
+++ src/coreLib/options/AuxiliaryOutputFlag.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nmfs.Agepro.CoreLib
+{
+  public enum AuxiliaryOutputFlag
+  {
+    OutfileNoStockExcludeStockAux = 0,
+    OutfileAppendStockAllAux = 1,
+    OnlyOutfileNoStock = 2,
+    OnlyOutfileAppendStock = 3,
+    OutfileAppendStockExcludeStockAux = 4
+  }
+
+  public interface IStockSummary
+  {
+    AuxiliaryOutputFlag SummaryAuxFileOutputFlag { get; }
+  }
+}
